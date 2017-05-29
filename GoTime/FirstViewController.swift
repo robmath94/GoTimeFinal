b63a36e5205f51a65b62bc2915d431ec6bf38dab
Robert Mathews <robertm94@cox.net>
2017-05-29 13:35:53 -0700

    Initial Commit

---
+++ GoTime/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  GoTime
+//
+//  Created by Robert Mathews on 5/29/17.
+//  Copyright © 2017 Robert Mathews. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+   override func viewDidLoad() {
+      super.viewDidLoad()
+      // Do any additional setup after loading the view, typically from a nib.
+   }
+
+   override func didReceiveMemoryWarning() {
+      super.didReceiveMemoryWarning()
+      // Dispose of any resources that can be recreated.
+   }
+
+
+}
+
---
+++ GoTimeTests/GoTimeTests.swift
@@ -0,0 +1,36 @@
+//
+//  GoTimeTests.swift
+//  GoTimeTests
+//
+//  Created by Robert Mathews on 5/29/17.
+//  Copyright © 2017 Robert Mathews. All rights reserved.
+//
+
+import XCTest
+@testable import GoTime
+
+class GoTimeTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
